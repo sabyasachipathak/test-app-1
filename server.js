f557a4b3858c17a19df4a5aaf35454e31d8e1214
Sabyasachi <sabyasachipathak13@gmail.com>
2020-05-04 21:46:16 +0530

    changed path.join in server js from path from path.resolve

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-//Seve static assets in production
+//Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   //set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (res, req) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
